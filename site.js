8960caba36264b1126bef621425c96f2c87c491a
Josh Ghent <me@joshghent.com>
2016-01-21 16:22:25 +0000

    Added basic template of about section. Commented JS

--- site.js
+++ site.js
@@ -5,5 +5,7 @@ $(document).ready(function(){
     var current_year = year_obj.getFullYear();
     var copyright_symbol = '\u00A9'
     
+    // This will add a copyright notice to the bottom of the site in the footer. We get the current year by using the native getFullYear() method 
+    // SAMPLE OUTPUT: C Josh Ghent 2016
     $("#copyright_year").text(copyright_symbol + ' Josh Ghent ' + current_year);
 });
